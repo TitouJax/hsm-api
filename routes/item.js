c945b631ba980de3b805c259956dfbb0533892e2
TitouJax <tristan.gadaud@gmail.com>
2021-04-22 14:52:54 +0300

    fixed call error

--- routes/item.js
+++ routes/item.js
@@ -2,27 +2,17 @@ const router = require('express').Router();
 const db = require('../db/database');
 
 router.get('/:item', (req, res) => {
-    db.db.getConnection(function(err, connection) {
-        if (err) {
-            return res.send({error: err});
-        }
         db.getItemByName(req.params.item, callback => {
             if (callback.error) return res.send(callback);
             else return res.send(callback);
         })
-    })
 });
 
 router.get('/', (req, res) => {
-    db.db.getConnection(function(err, connection) {
-        if (err) {
-            return res.send({error: err});
-        }
         db.getAllItem(callback => {
             if (callback.error) return res.send(callback);
             else return res.send(callback);
         })
-    })
 })
 
 module.exports = router;
