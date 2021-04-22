fb71cb05c02d41ebe0fd70057d44165ecb3662ec
TitouJax <tristan.gadaud@gmail.com>
2021-04-22 14:59:13 +0300

    fixed call error

--- routes/auth.js
+++ routes/auth.js
@@ -4,43 +4,25 @@ const db = require('../db/database');
 
 router.post('/register', (req, res) =>
 {
-    db.db.getConnection(function(err, connection) {
-        if (err) {
-            callback({error: err});
-            return;
-        }
-        db.createUser(req.body, callback => {
-            if (callback.error) return res.send(callback);
-            else return res.send(callback);
-        })
+    db.createUser(req.body, callback => {
+        if (callback.error) return res.send(callback);
+        else return res.send(callback);
     })
 });
 
 router.post('/login', (req, res) =>
 {
-    db.db.getConnection(function(err, connection) {
-        if (err) {
-            callback({error: err});
-            return;
-        }
-        db.loginUser(req.body, callback => {
-            if (callback.error) return res.send(callback);
-            else return res.send(callback);
-        })
+    db.loginUser(req.body, callback => {
+        if (callback.error) return res.send(callback);
+        else return res.send(callback);
     })
 });
 
 router.get('/profile/:name', (req, res) => {
-    db.db.getConnection(function(err, connection) {
-        if (err) {
-            callback({error: err});
-            return;
-        }
         db.getOrdersByUser(req.params.name, callback => {
             if (callback.error) return res.send(callback);
             else return res.send(callback);
         })
-    })
 });
 
 module.exports = router;
--- routes/orders.js
+++ routes/orders.js
@@ -3,27 +3,15 @@ const tokenValidation = require('../validation/tokenValidation');
 const db = require("../db/database");
 
 router.post('/create', tokenValidation.auth, (req, res) => {
-    db.db.getConnection(function(err, connection) {
-        if (err) {
-            callback({error: err});
-            return;
-        }
         db.createOrder(req.body, req.user.email, callback => {
             res.send(callback);
         });
-    })
 })
 
 router.delete('/delete', tokenValidation.auth, (req, res) => {
-    db.db.getConnection(function(err, connection) {
-        if (err) {
-            callback({error: err});
-            return;
-        }
         db.deleteOrderByEmailAndId(req.user.email, req.body.id, call => {
             res.send(call);
         });
-    })
 })
 
 router.get('/:item', (req, res) => {
